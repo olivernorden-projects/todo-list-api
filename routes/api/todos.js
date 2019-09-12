d5d1029fcf945cbf62b1f0d6eb45e6400b5d846d
Oliver Nordén <oliver@tapirens.se>
2019-09-12 18:36:56 +0200

    Route to add new todo

--- routes/api/todos.js
+++ routes/api/todos.js
@@ -4,13 +4,33 @@ const router = express.Router();
 // Import Todo model
 const Todo = require('../../models/todos');
 
-// @route   GET api/vehicles
-// @desc    Get user's vehicles
-// @access  Private
+// @route   GET api/todos
+// @desc    Get all todos
+// @access  Public
 router.get('/', (req, res) => {
     Todo.find()
         .sort({ date: -1 })
         .then( todos => res.json(todos));
+});
+
+// @route   POST api/todos
+// @desc    Add a new todo
+// @access  Public
+router.post('/', (req, res) => {
+    const { todo } = req.body;
+
+    // Validation 
+    if (!todo) {
+        res.status(400).json({ msg: "Please enter all field" });
+        return;
+    }
+
+    const newTodo = new Todo({
+        todo
+    })
+
+    newTodo.save()
+        .then(todo => res.json(todo));
 });
 
 module.exports = router;
